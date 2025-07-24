5cc2e580c7821bb8e6816b0beb1f5c9d4166ab55
MRT-800(Abhi) <abhishekthapaliya81@gmail.com>
2025-07-24 12:45:25 +0545

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+/*
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); // Waits for key press
+        }
+    }
+}*/
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Name();
+            person.Display();
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Name
+    {
+        private  int age = 20;
+        private  string name = "ABHISHEK";
+        private  string address = "Kathmandu";
+        private  double height = 5.5;
+        private  bool isStudent = true;
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Is Student: " + isStudent);
+        }
+    }
+}
+
